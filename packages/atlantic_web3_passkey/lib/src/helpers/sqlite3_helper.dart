bb45c7876b750dc9240e3ede90a0484e492ec2f6
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2024-06-19 08:21:17 -0400

    mejorar sqlite

--- packages/atlantic_web3_passkey/lib/src/helpers/sqlite3_helper.dart
+++ packages/atlantic_web3_passkey/lib/src/helpers/sqlite3_helper.dart
@@ -8,9 +8,12 @@ final class Sqlite3Helper {
   static String dateToSqlite(DateTime input) => input.toString();
 
   static DateTime sqliteToDate(String input) {
-    List<String> result = input.substring(0, 10).split('-');
-    return DateTime(int.parse(result.elementAt(0)),
-        int.parse(result.elementAt(1)), int.parse(result.elementAt(2)));
+    if (input.isDate()) {
+      return DateTime.parse(input);
+    } else {
+      List<String> result = input.substring(0, 10).split('-');
+      return DateTime(int.parse(result.elementAt(0)),
+          int.parse(result.elementAt(1)), int.parse(result.elementAt(2)));
+    }
   }
-
 }
